eb1447d07b9494976de58b04e994969d096f8630
KCable <126310715+KCable@users.noreply.github.com>
2023-09-24 06:56:20 -0700

    Created DisplayEntries Components and Instantiated Component on App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+
 
 function App() {
+
+  const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"}, {weight: 176, date: "11-24-2021"}])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayEntries parentEntries={entries} />
+      
     </div>
   );
 }
---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,27 @@
+
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry, index) => {
+            return (
+              <tr>
+                <td>{index +1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+              </tr>
+            )
+          })}
+        </tbody>  
+      </table> 
+     );
+}
+ 
+export default DisplayEntries;
